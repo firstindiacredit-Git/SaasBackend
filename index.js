e01fe7964c6f7071c36e9c329e7eafca3b7ed5c9
Sinod Kumar <sinodkr001@gmail.com>
2024-11-11 17:41:01 +0530

    some changes

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { exec } = require("child_process");
 //const cloudconvert = new (require('cloudconvert'))('eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNTIyOWNlYjRkYzAxMDUxZGU4YzEzZWY3MjY4YzRjYjMxYjg4ODhiYTFiODM5M2UwYzRjM2QxZGJkOTViMzc3MWE0NTE2YTUyMzEwYTBhOGEiLCJpYXQiOjE3Mjg5MDIxMTMuMjU5MzIyLCJuYmYiOjE3Mjg5MDIxMTMuMjU5MzIzLCJleHAiOjQ4ODQ1NzU3MTMuMjUyNTY4LCJzdWIiOiI2OTg3NTY4OSIsInNjb3BlcyI6W119.ImpTJP6IyQuGcpKNtbfnXGm4vbGAJXwfHvZDXPjy5qLoUgTh8nnd4x9oLH1gTtBRUcyuTs9RrDutG6dMt3DybYtLwThUToH9yvaGN0bJJ9t-xTKdPHC3gFW2xA28mVKCrjOZngAmPWfD2VnZXhJA6M3a7OYyWWDN8k3ZFcAfkzZJrhlUtFP3XuETu7hpGew81X8Nnya95S99F8SzV2BDK1xZRxjFXRU9MPaC-FSkHyGHRZPrcUQU-6yzU4RjcRnId96mswPou-2vRfY98jPu9ZzC9gGMFPGtFXbv6b0n82Qsnjrayzu26H4RGg47oNk4sD7XUcmjsjsryhOE4aXKdqTZfBr2VGt3ekWCcLg1bs_FtgptVgvv1YACo7D00C8wmEq3JKfii_AplayPtxXCRl6iZ-WI4tdkUzPG-QCu58jx-Fq7Divuu4w1BkNUeU9iFFi-55pbXm6Qwrirl6HnleXTGsf85xPk1kdlENK4QIPAk838cBatghbfCvXmOEbf5ECO8x-7i8_39n4R8YqqHwf6Wtue3Do7GRZLmUVMatPi7HHv1gaUSGNVH94ydrwb0LsqLA9KtQ5mYP_3Y4B_5w1uvBbykhvWttaPRATaHlpoNPUxIaTShcHyyZSGuv2NGkP_qzbhfNBWSE0Z3-Qt8TZpAxeqJH9vJZZp4r4YLjk');
 const fs = require('fs');
 const path = require('path');
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -65,8 +67,8 @@ app.post("/protect", upload.single("pdfFile"), (req, res) => {
   
   
   
-  app.listen(8080, () => {
-    console.log('CORS Proxy Server running on port 8080');
+  app.listen(process.env.PORT, () => {
+    console.log(`CORS Proxy Server running on port ${process.env.PORT}`);
   });
   
   
