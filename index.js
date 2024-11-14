c28744f7c3034b10e0d2c744b84069eb8168f7d9
Sinod Kumar <sinodkr001@gmail.com>
2024-11-14 16:58:53 +0530

    fff

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
+// require("dotenv").config();
+// const express = require("express");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const fileUpload = require("express-fileupload");
+// const protectRoute = require("./routes/protect.route.js");
+// const unlockRoute = require("./routes/unlock.route.js");
+// const emailSenderRoute = require("./routes/emailsender.route.js");
+// const pdfToWordRoute = require("./routes/pdftoword.route.js");
+// const emailCheckerRoute = require("./routes/emailchecker.route.js");
+
+
+// const app = express();
+// const PORT = 4000;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
+// app.use((err, req, res, next) => {
+//     res.status(err.code || 500).json({ message: err.message || "Internal Server Error" });
+//   });
+
+// app.use("/protect", protectRoute);
+// app.use("/unlock-pdf", unlockRoute);
+// app.use("/send-email", emailSenderRoute);
+// app.use("/api/file", pdfToWordRoute);
+// app.use("/validate", emailCheckerRoute);
+
+// app.get("/", (req, res) => {
+//     res.send("Welcome to SaasBackend");
+//   });
+
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const fileUpload = require("express-fileupload");
 const protectRoute = require("./routes/protect.route.js");
 const unlockRoute = require("./routes/unlock.route.js");
 const emailSenderRoute = require("./routes/emailsender.route.js");
 const pdfToWordRoute = require("./routes/pdftoword.route.js");
 const emailCheckerRoute = require("./routes/emailchecker.route.js");
-
 
 const app = express();
 const PORT = 4000;
@@ -18,22 +56,23 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp/" }));
-app.use((err, req, res, next) => {
-    res.status(err.code || 500).json({ message: err.message || "Internal Server Error" });
-  });
 
+// Routes
 app.use("/protect", protectRoute);
 app.use("/unlock-pdf", unlockRoute);
 app.use("/send-email", emailSenderRoute);
 app.use("/api/file", pdfToWordRoute);
 app.use("/validate", emailCheckerRoute);
 
+// Root route
 app.get("/", (req, res) => {
     res.send("Welcome to SaasBackend");
-  });
+});
 
-
+// Error handling middleware
+app.use((err, req, res, next) => {
+    res.status(err.code || 500).json({ message: err.message || "Internal Server Error" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
--- routes/protect.route.js
+++ routes/protect.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const multer = require("multer");
 const { protectPDF } = require("../controller/protect.controller");
-//const { protectPDF } = require("./controller/protect.controller.js");
 
 const router = express.Router();
 const upload = multer({ dest: "uploads/" });
